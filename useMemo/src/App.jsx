430ccf7cc19e694ed5ceadbfb88e2d11da886a57
Umapathi2004 <umapathi1014@gmail.com>
2024-11-01 14:40:33 +0530

    Changed

---
+++ useMemo/src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import UseMemo from './UseMemo'
+
+const App = () => {
+  return (
+    <UseMemo/>
+  )
+}
+
+export default App
---
+++ useMemo/src/UseMemo.jsx
@@ -0,0 +1,37 @@
+import React, { Fragment, useState, useMemo, useEffect } from 'react';
+
+const UseMemo = () => {
+  const [dark, setDark] = useState(false);
+  const [num, setNum] = useState(0);
+
+  const style = {
+    color: dark ? "#ffff" : "black",
+    backgroundColor: dark ? "black" : "#ffff"
+  };
+
+  // Memoize the result of numResult
+  const result = useMemo(() => {
+    console.log("Usememo")
+    return numResult(num)
+}, [num]);
+// const result = numResult(num)
+  return (
+    <Fragment>
+      <input
+        type="number"
+        value={num}
+        onChange={(e) => setNum(Number(e.target.value))}
+      />
+      <div className="result" style={style}>{result}</div>
+      <button onClick={() => setDark((prev) => !prev)}>Toggle Theme</button>
+    </Fragment>
+  );
+};
+
+// Simulate an expensive calculation
+const numResult = (val) => {
+  for (let i = 0; i <= 1000000000; i++) {} // reduced for efficiency
+  return val * 2;
+};
+
+export default UseMemo;
