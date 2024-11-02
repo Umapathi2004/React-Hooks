af96d860dc6442a83d4111e35372b8a6d447db64
Umapathi2004 <umapathi1014@gmail.com>
2024-11-02 19:28:49 +0530

    Changed

--- Redux/src/App.jsx
+++ Redux/src/App.jsx
@@ -1,8 +1,25 @@
-import React from 'react'
-
+import React, { Fragment, useState } from 'react'
+import { useSelector,useDispatch } from 'react-redux'
+import { increment,decrement,incrementByNum,reset } from './operations/counter/counter'
 const App = () => {
+  const count = useSelector((state)=>state.counter.count)
+  const dispatch = useDispatch()
+
+  const [num,setNum] = useState(0);
+  const AddNum = () =>{
+    dispatch(incrementByNum(Number(num) || 0))
+  }
   return (
-    <div>App</div>
+    <Fragment>
+    <div style={{display:"flex",gap:"20px",fontSize:"2em"}}>
+      <button onClick={()=>dispatch(increment())} style={{fontSize:"1em"}}>+</button>
+      <div>{count}</div>
+      <button onClick={()=>dispatch(decrement())} style={{fontSize:"1em"}}>-</button>
+    </div>
+    <input type="number" onChange={(e)=>setNum(e.target.value)}/>
+    <button onClick={()=>dispatch(reset())}>Reset</button>
+    <button onClick={AddNum}>Add Num</button>
+    </Fragment>
   )
 }
 
